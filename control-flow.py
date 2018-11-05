f86d0e2e34d4fc0f8478e8937f16d2cd458e57de
Bijay Yadav <bijay.yadav@cloudyfox.com>
2018-11-05 15:34:48 +0545

    modified, see comment inline in code:   control-flow.py

--- control-flow.py
+++ control-flow.py
@@ -6,7 +6,37 @@ if x < 0:
     print('Negative changed to zero')
     print('Zero')
 elif x == 1:
-     print('Single')
+    print('Single')
 else:
     print('More')
 print("done.")
+
+''' Output: 
+Please enter an integer: 24
+More
+done.
+'''
+# for loop
+
+words = ['cat', 'window', 'defenestrate']
+
+for w in words:
+    print(w)
+
+''' Output:
+cat
+window
+defenestrate
+'''
+
+for w in words[:]:  # Loop over a slice copy of the entire list.
+    print(w)
+
+''' Output:
+cat
+window
+defenestrate
+'''
+
+# len of words
+print("len of words: ", len(words))
